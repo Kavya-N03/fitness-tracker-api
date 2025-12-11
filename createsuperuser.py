4323f18e8cccbe35c518f7d7012fd44267aa2e90
Kavya N <nkavya649@gmail.com>
2025-12-11 14:10:55 +0530

    added admin

--- createsuperuser.py
+++ createsuperuser.py
@@ -1,22 +1,27 @@
 import os
 import django
 
-# Set the correct settings module for your project
+# Load Django settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fitness_tracker_api.settings")
-
-# Initialize Django
 django.setup()
 
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = "admin"
-email = "admin@example.com"
-password = "Admin@123"
+username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print("Superuser created successfully!")
+if username and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+        print("Superuser created successfully!")
+    else:
+        print("Superuser already exists. Skipping creation.")
 else:
-    print("Superuser already exists.")
+    print("Superuser environment variables not set.")
